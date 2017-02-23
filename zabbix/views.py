0a3d729b438c3fad3766f1f6846b0c45a8ecc122
张磊 <zhanglei@tansuotv.com>
2017-02-23 11:13:40 +0800

    no message

--- zabbix/views.py
+++ zabbix/views.py
@@ -271,7 +271,7 @@ def _wx_post(request,wxcpt):
 			if Msg_dick['EventKey'] == 'show_help':
 				ResContent = _show_help(Msg_dick['FromUserName'])
 			elif Msg_dick['EventKey'] == 'traf_dsp':
-				ResContent = _show_traf(Msg_dick,'DSP')
+				ResContent = _show_traf('DSP')
 
 	res = ResData(wxcpt,Msg_dick['ToUserName'],Msg_dick['FromUserName'],Msg_dick['CreateTime'],ResContent,sReqNonce,sReqTimeStamp)
 	return HttpResponse(res)
