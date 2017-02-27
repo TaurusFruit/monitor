963a6f02d1e2166f2834dbd0d0e51600daba3e7b
张磊 <zhanglei@tansuotv.com>
2017-02-27 09:47:46 +0800

    修改url连接

--- zabbix/urls.py
+++ zabbix/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
 	url(r'^wx_api/hostimg/(?P<graphid>\d+)',views.hostimg,name='hostimg'),
 
 	#显示主机组流量图
-	url(r'^wx_api/netwrok/(?P<group_name>\w+)/(?P<eth_id>\d)$',views.HostNewrok,name='hostnework'),
+	url(r'^wx_api/network/(?P<group_name>\w+)/(?P<eth_id>\d)$',views.HostNewrok,name='hostnework'),
 
 	# url(r'^wx_api/img/(?P<stime>\d+)/(?P<itemid>\d+)',views.img),
 	url(r'^wx_api/cacti_graf',views.cacti_graf),
